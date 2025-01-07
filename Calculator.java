0886e50daf099d357473dba3abf772d5f680bdd6
Sssrajputt <149288600+Sssrajputt@users.noreply.github.com>
2025-01-07 13:32:38 +0530

    add functionality

--- Calculator.java
+++ Calculator.java
@@ -3,5 +3,13 @@ public class Calculator {
     public static void main(String[] args) {
           System.out.println("Welcome");
 
+
+Scanner sc= new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+ System.out.println(a+b);
+
+
+
 }
 }
