9da8bb6f001d0437b274783edd6fb5a0a022caa2
Sssrajputt <149288600+Sssrajputt@users.noreply.github.com>
2025-01-07 14:50:49 +0530

    substraction

--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,32 @@ public class Calculator {
 Scanner sc= new Scanner(System.in);
 int a = sc.nextInt();
 int b = sc.nextInt();
- System.out.println(a+b);
+    // Display menu
+        System.out.println("\nChoose an operation:");
+        System.out.println("1. Addition (+)");
+        System.out.println("2. Subtraction (-)");
+       
+          System.out.print("Enter your choice (1-5): ");
+        int choice = scanner.nextInt();
+
+        double result;
+
+        // Perform operation using switch
+        switch (choice) {
+                case 1:
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+                   case 2:
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+                default:
+                System.out.println("Invalid choice! Please select a valid operation.");
+        }
+
+        scanner.close();
+
 
 
 
